184f684991b81a16ffc041f43f6c203f8d0c8030
david1707 <DavidAlacant@gmail.com>
2020-04-12 12:53:53 +0200

    Third lesson

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,8 @@ const rootPath = require("../utils/rootPath");
 
 const router = express.Router();
 
+const memories = [];
+
 // router.get('/', (req, res, next) => {
 //     console.log('HOME')
 //     res.send('<html><head><title>Home</title></head><body><h1>Home</h1></body></html>')
@@ -21,14 +23,19 @@ const router = express.Router();
 // })
 
 router.get("/", (req, res, next) => {
-  console.log("HOME");
+  console.log(memories);
   res.sendFile(path.join(rootPath, "views", "home.html"));
 });
 
 router.get("/create-memory", (req, res, next) => {
-  console.log("CREATE MEMORY");
   res.sendFile(path.join(rootPath, "views", "create-memory.html"));
 });
+
+router.post('/create-memory', (req, res, next) => {
+  const postedTitle = req.body.title
+  memories.push({title: postedTitle})
+  res.redirect('/create-memory')
+})
 
 router.use((req, res, next) => {
   res.status(404).sendFile(path.join(rootPath, "views", "404.html"));
