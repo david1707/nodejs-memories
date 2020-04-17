5593a103a239b47469fd59af500f7c8aed50be46
david1707 <DavidAlacant@gmail.com>
2020-04-17 18:38:56 +0200

    [Memory] Create and GetAll with MongoDB

--- models/memory.js
+++ models/memory.js
@@ -1,85 +1,31 @@
-const fs = require("fs");
-const path = require("path");
+const getDB = require("../utils/databaseConfig").getDB;
 
-const filePath = path.join(
-  path.dirname(process.mainModule.filename),
-  "data",
-  "memories.json"
-);
-
-module.exports = class Memory {
-  constructor(title, imageUrl, gps, comment, id) {
-    if (id) {
-      this.id = id;
-    } else {
-      this.id = Math.random();
-    }
+class Memory {
+  constructor(title, imageUrl, gps, comment) {
     this.title = title;
     this.imageUrl = imageUrl;
     this.gps = gps;
     this.comment = comment;
   }
 
-  save(callback) {
-    fs.readFile(filePath, (err, response) => {
-      let memories = [];
-      if (!err) {
-        memories = JSON.parse(response);
-      }
-      memories.push(this);
-      fs.writeFile(filePath, JSON.stringify(memories), (err) => { 
-        console.log(err);
-      });
-      return callback(this.id)
-    });
+  save() {
+    const db = getDB();
+    return db
+      .collection("memories")
+      .insertOne(this)
+      .then((result) => result)
+      .catch((err) => console.log(err));
   }
 
-  update(memoryID) {
-    fs.readFile(filePath, (err, response) => {
-      let memories = [];
-      if (!err) {
-        memories = JSON.parse(response);
-      }
-      const memoryIndex = memories.findIndex((memory) => memory.id == memoryID);
-      memories[memoryIndex] = this;
-
-      fs.writeFile(filePath, JSON.stringify(memories), (err) =>
-        console.log(err)
-      );
-    });
+  static getMemories() {
+    const db = getDB();
+    return db
+      .collection("memories")
+      .find()
+      .toArray()
+      .then((result) => result)
+      .catch((err) => console.log(err));
   }
+}
 
-  static getMemories(callback) {
-    fs.readFile(filePath, (err, response) => {
-      if (err) {
-        return callback([]);
-      }
-      return callback(JSON.parse(response));
-    });
-  }
-
-  static getMemory(memoryID, callback) {
-    fs.readFile(filePath, (err, response) => {
-      if (err) {
-        return callback([]);
-      }
-      const memories = JSON.parse(response);
-      const memory = memories.find((memory) => memory.id == memoryID);
-      return callback(memory);
-    });
-  }
-
-  static deleteMemory(memoryID, callback) {
-    fs.readFile(filePath, (err, response) => {
-      let memories = [];
-      if (!err) {
-        memories = JSON.parse(response);
-      }
-      memories = memories.filter((memory) => memory.id != memoryID);
-
-      fs.writeFile(filePath, JSON.stringify(memories), (err) => {
-        console.log(err);
-      });
-    });
-  }
-};
+module.exports = Memory;
