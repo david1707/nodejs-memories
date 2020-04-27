372f206b9b51f3fff9790054e3ee103e9bb57b3b
david1707 <DavidAlacant@gmail.com>
2020-04-27 12:40:05 +0200

    Flash messages

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+var flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 
@@ -13,6 +15,20 @@ app.set("views", "views");
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "static")));
+app.use(
+  session({
+    secret: "my_secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+// Global variables
+app.use(function (req, res, next) {
+  res.locals.messages = req.flash("messages");
+  next();
+});
 
 // Routes
 const routes = require("./routes/routes");
@@ -22,7 +38,12 @@ app.use(routes);
 mongoose
   .connect(
     "mongodb+srv://david:nonono33@cluster0-qnzrq.mongodb.net/tutorial?retryWrites=true&w=majority",
-    { useUnifiedTopology: true, useNewUrlParser: true }
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
   )
   .then((result) => {
     app.listen(3000);
